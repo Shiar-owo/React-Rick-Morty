ba10591ee3cf77717ae5ab03d3eb6b1cd55ad501
Shiar owo <jhairtoc@gmail.com>
2026-06-14 10:26:11 -0500

    style: replace favicon and navbar logo with pickle.png

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,8 +5,8 @@ export default function Navbar() {
   return (
     <nav className="navbar">
       <NavLink to="/" className="navbar__brand">
-        <span className="navbar__logo">🌀</span>
-        <span className="navbar__title">Rick & Morty</span>
+        <img src="/pickle.png" alt="Pickle Rick" className="navbar__logo" width="32" height="32" />
+        <span className="navbar__title">Rick & Morty API</span>
       </NavLink>
       <div className="navbar__links">
         <NavLink to="/" end className={({ isActive }) => `navbar__link ${isActive ? 'navbar__link--active' : ''}`}>
