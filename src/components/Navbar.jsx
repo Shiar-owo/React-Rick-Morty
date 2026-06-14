fbf10b4edaad481b315601ecb57da9fdf8cfaac3
Shiar owo <jhairtoc@gmail.com>
2026-06-14 09:39:41 -0500

    feat: add Navbar component with routing links

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import { NavLink } from 'react-router-dom';
+import './Navbar.css';
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <NavLink to="/" className="navbar__brand">
+        <span className="navbar__logo">🌀</span>
+        <span className="navbar__title">Rick & Morty</span>
+      </NavLink>
+      <div className="navbar__links">
+        <NavLink to="/" end className={({ isActive }) => `navbar__link ${isActive ? 'navbar__link--active' : ''}`}>
+          Characters
+        </NavLink>
+        <NavLink to="/favorites" className={({ isActive }) => `navbar__link ${isActive ? 'navbar__link--active' : ''}`}>
+          Favorites
+        </NavLink>
+      </div>
+    </nav>
+  );
+}
